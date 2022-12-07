b9be84e933092f5563d41a4345774ac000e5ce99
alind7402 <alind7402@gmail.com>
2022-12-07 09:49:37 -0500

    taskscheuleR work

--- analysis/getstationstatus.R
+++ analysis/getstationstatus.R
@@ -3,7 +3,7 @@ library(gbfs)
 library(dplyr)
 
 #change the file name when running on local machine
-filename="C:/Users/vvexi/OneDrive/Documents/R Scripts/DS241-Class-Project/analysis"
+filename="U:/DS241-Class-Project/analysis/station_info.csv"
 
 station_status <- get_station_status("cabi") %>% 
   select(station_id, num_docks_available, num_docks_disabled, 
